a73a666dfa789db482ccccfe522f2bc395545e87
aparannha123 <aparannharoy@gmail.com>
2023-06-16 20:33:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Radio from "./Radio";
+import "./app.scss";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        <h1>APA Radio Player</h1>
+        <h2>Pick a genre, choose a station, start listening</h2>
+      </div>
+      <Radio/>
+    </>
   );
 }
 
